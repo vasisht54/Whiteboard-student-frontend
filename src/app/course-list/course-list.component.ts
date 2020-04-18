968abb1d5db9e10a20f0266bab78b6de3e0293e0
vasisht54 <vasisht.theviper@gmail.com>
2020-04-18 01:01:30 -0400

    Added courst-list component

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -9,6 +9,13 @@ export class CourseListComponent implements OnInit {
 
   constructor() { }
 
+  courses = [
+    {_id: '123', title: 'Course A'},
+    {_id: '234', title: 'Course B'},
+    {_id: '345', title: 'Course C'},
+    {_id: '456', title: 'Course D'},
+  ];
+
   ngOnInit(): void {
   }
 
